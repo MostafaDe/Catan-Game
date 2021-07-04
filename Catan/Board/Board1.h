945d554bef17e2da86bc6b713c9657d69331d60f
ARMAN-SH <81294934+ARMAN-SH@users.noreply.github.com>
2021-07-04 19:36:50 +0430

    Update Board1.h

--- Catan/Board/Board1.h
+++ Catan/Board/Board1.h
@@ -4,18 +4,16 @@
 
 #ifndef SETTINGUPGOOGLETEST_BOARD1_H
 #define SETTINGUPGOOGLETEST_BOARD1_H
-#include <gtest/gtest.h>
-#include <gmock/gmock.h>
 #include "Board.h"
 class Board1 : private Board  {
 public:
     void setRandomNumberToLands() ;
 
-     bool BuildHouse(int postion);
-    bool BuildRoad(int postion );
-    bool BuilBigCity(int postion);
-    bool BuildTown(int postion);
-     bool BuildBridge(int postion);
+     //bool BuildHouse(int postion,Color color);
+    bool BuildRoad(int postion ,Color color);
+    bool BuilBigCity(int postion,Color color);
+    bool BuildTown(int postion,Color color);
+     bool BuildBridge(int postion,Color color);
     vector<map<Color,int>> calculateScore(int SumOfDices) ;
 
 };
