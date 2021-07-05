a039810b9977db2e5425fd9628f045de90ab6f1a
Mostafa Derispour <mostafa00734@gmail.com>
2021-07-05 20:10:50 +0430

    Server Folder and Its Classes commited to the project

---
+++ Catan/Server/Bank.cpp
@@ -0,0 +1,5 @@
+//
+// Created by apadana on ۰۵/۰۷/۲۰۲۱.
+//
+
+#include "Bank.h"
---
+++ Catan/Server/Bank.h
@@ -0,0 +1,16 @@
+//
+// Created by apadana on ۰۵/۰۷/۲۰۲۱.
+//
+
+#ifndef PLAYERS_BANK_H
+#define PLAYERS_BANK_H
+
+
+
+class Bank {
+
+};
+
+
+
+#endif //PLAYERS_BANK_H
