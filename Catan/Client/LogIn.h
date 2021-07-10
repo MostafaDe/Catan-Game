0130a47f415f11550e81f94b286b8bb1d0fd2dbe
ARMAN <shamsi.mohammad.arman@gmail.com>
2021-07-10 15:28:52 +0430

    update the LogIn.cpp and LogIn.h

--- Catan/Client/LogIn.h
+++ Catan/Client/LogIn.h
@@ -1,8 +1,41 @@
-//
-// Created by apadana on ۰۵/۰۷/۲۰۲۱.
-//
+#ifndef LOGIN_H
+#define LOGIN_H
 
-#ifndef PLAYERS_LOGIN_H
-#define PLAYERS_LOGIN_H
+#include <QDialog>
+#include <QTcpSocket>
+#include<QByteArray>
+#include<QJsonArray>
+#include<QJsonObject>
+#include<QLineEdit>
+#include <QDataStream>
+#include<QHostAddress>
+#include<QMessageBox>
+#include<QLabel>
+#include<QGridLayout>
+#include<QPushButton>
+#include<QDialogButtonBox>
+#include<QGuiApplication>
+#include<QJsonDocument>
+#include"player/player.h"
 
-#endif //PLAYERS_LOGIN_H
+class LogIn : public QDialog
+{
+    Q_OBJECT
+
+public:
+    LogIn(QWidget *parent = nullptr);
+
+    Player *getPlayer() const;
+    void setPlayer(Player *newPlayer);
+private slots:
+    void sendMessage();
+    void enableOkButton();
+    void read();
+
+private:
+    QTcpSocket *tcpSocket = nullptr;
+    QLineEdit*password,*userName;
+    QPushButton*ok,*cancel;
+    Player* player;
+};
+#endif // LOGIN_H
