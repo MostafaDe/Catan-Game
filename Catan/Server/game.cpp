b9084f74b746287754ad68a52850a7f558093c42
Mostafa Derispour <mostafa00734@gmail.com>
2021-07-13 23:12:28 +0430

    updating game class

--- Catan/Server/game.cpp
+++ Catan/Server/game.cpp
@@ -2,18 +2,13 @@
 
 
 
-Game::Game(QVector<Player> _players)
+Game::Game()
 {
-players = _players;
 
 
 }
 
-QJsonObject Game::doSomething(QJsonObject message, QVector<int> &socketDescriptors)
-{
-    QJsonObject jsObj;
-    return jsObj;
-}
+
 
 QJsonObject Game::stopGame(QVector<int> &socketDescriptors)
 {
@@ -28,3 +23,62 @@ QJsonObject Game::getBoardInformation(QVector<int> &socketDescriptors)
     return jsObj;
 
 }
+
+QJsonObject Game::buildHouse(QJsonObject message, QVector<int> &socketDescriptors)
+{
+    QJsonObject jsObj;
+    return jsObj;
+}
+
+QJsonObject Game::buildRoad(QJsonObject message, QVector<int> &socketDescriptors)
+{
+
+    QJsonObject jsObj;
+    return jsObj;
+}
+
+QJsonObject Game::buildBridge(QJsonObject message, QVector<int> &socketDescriptors)
+{
+    QJsonObject jsObj;
+    return jsObj;
+}
+
+QJsonObject Game::transaction(QJsonObject message, QVector<int> &socketDescriptors)
+{
+    QJsonObject jsObj;
+    return jsObj;
+}
+
+QJsonObject Game::buyCard(QJsonObject message, QVector<int> &socketDescriptors)
+{
+    QJsonObject jsObj;
+    return jsObj;
+}
+
+QJsonObject Game::playCard(QJsonObject message, QVector<int> &socketDescriptors)
+{
+    QJsonObject jsObj;
+    return jsObj;
+}
+
+QJsonObject Game::endOfTurn(QJsonObject message, QVector<int> &socketDescriptors)
+{
+    QJsonObject jsObj;
+    return jsObj;
+}
+
+QJsonObject Game::movingThief(QJsonObject message, QVector<int> &socketDescriptors)
+{
+    QJsonObject jsObj;
+    return jsObj;
+}
+
+QJsonObject Game::winner(bool &iswinner, QVector<int> &socketDescriptors)
+{
+    QJsonObject jsObj;
+    return jsObj;
+}
+
+
+
+
--- Catan/Server/game.h
+++ Catan/Server/game.h
@@ -6,18 +6,31 @@
 #include<QVector>
 #include<QJsonDocument>
 #include<QJsonObject>
+#include"Board/Board1.h"
 class Game
 {
 public:
-    Game(QVector<Player> _players);
-    QJsonObject doSomething(QJsonObject message,QVector<int>& socketDescriptors);
+    Game();
+    QJsonObject winner(bool& iswinner,QVector<int>& socketDescriptors);
     QJsonObject stopGame(QVector<int>& socketDescriptors);
     QJsonObject getBoardInformation(QVector<int>& socketDescriptors);
+    QJsonObject buildHouse(QJsonObject message ,QVector<int>& socketDescriptors);
+    QJsonObject buildRoad(QJsonObject message ,QVector<int>& socketDescriptors);
+    QJsonObject buildBridge(QJsonObject message ,QVector<int>& socketDescriptors);
+    QJsonObject transaction(QJsonObject message ,QVector<int>& socketDescriptors);
+    QJsonObject buyCard(QJsonObject message ,QVector<int>& socketDescriptors);
+    QJsonObject playCard(QJsonObject message ,QVector<int>& socketDescriptors);
+    QJsonObject endOfTurn(QJsonObject message ,QVector<int>& socketDescriptors);
+    QJsonObject movingThief(QJsonObject message ,QVector<int>& socketDescriptors);
+
+
 
 private:
-    QVector<Player> players;
-    Board* board;
+
 
 };
 
+
 #endif // GAME_H
+//Color::Red
+// Color numeOfColor = Color::;
