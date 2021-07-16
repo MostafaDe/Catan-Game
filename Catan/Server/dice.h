c60abf46ed62723cca41e67354adb4aca91132f4
Mostafa Derispour <mostafa00734@gmail.com>
2021-07-16 20:30:09 +0430

    update dice class

--- Catan/Server/dice.h
+++ Catan/Server/dice.h
@@ -7,6 +7,12 @@ class Dice
 public:
     Dice();
     void generateRandomDices();
+    int getDice1() const;
+    void setDice1(int newDice1);
+
+    int getDice2() const;
+    void setDice2(int newDice2);
+
 private:
     int dice1;
     int dice2;
